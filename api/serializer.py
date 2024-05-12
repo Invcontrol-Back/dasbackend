6901b8b81e21d169e5b0b17c5e6d01e1ef55ea78
KEVIN-XTREM-2023 <kevinxtrem2022@gmail.com>
2024-05-12 18:44:43 -0500

    Servicio login

--- api/serializer.py
+++ api/serializer.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
+from .models import *
 
 
+
+class RolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rol
+        fields = '__all__'  
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'  
+
+ 
--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+from api import views
+from .views import LoginView
+
+router = routers.DefaultRouter()
+#router.register(r'login', views.LoginView.as_view(), basename='login')
+router.register(r'usuario',views.UsuarioViewSet)
+router.register(r'rol',views.RolViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login/', views.LoginView.as_view(), name='login'),
+]
