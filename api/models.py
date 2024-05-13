ecf1f980d42b87a6bd57a1c660bc5ecb2d265030
KEVIN-XTREM-2023 <kevinxtrem2022@gmail.com>
2024-05-13 00:00:54 -0500

    Servicios crud Usuarios realizados

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class Rol(models.Model):
 class Usuario(models.Model):
     usu_id = models.AutoField(primary_key=True)
     usu_correo = models.CharField(max_length=30)
-    usu_contrasenia = models.CharField(max_length=30)
+    usu_contrasenia = models.CharField(max_length=300)
     usu_cedula = models.CharField(max_length=10)
     usu_nombres = models.CharField(max_length=30)
     usu_apellidos = models.CharField(max_length=30)
