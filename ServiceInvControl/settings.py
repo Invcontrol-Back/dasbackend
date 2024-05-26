39bb9dbaed21ea6ba22a996d2ea68dbd34d9dea0
KEVIN-XTREM-2023 <kevinxtrem2022@gmail.com>
2024-05-26 16:10:55 -0500

    Integracion de los cambios realizados de Usuario a Develop

--- ServiceInvControl/settings.py
+++ ServiceInvControl/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    
+    'corsheaders',
 
 ]
 
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ServiceInvControl.urls'
