e19f6ea4df8d589f9c208b4854796850b9f98b85
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-06-01 00:56:55 -0500

    repotencia
    repotencia

--- api/views.py
+++ api/views.py
@@ -411,6 +411,35 @@ class DetalleTecnologicoViewSet(viewsets.ModelViewSet):
     queryset = DetalleTecnologico.objects.all()
     serializer_class = DetalleTecnologicoSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        componente_id = kwargs.get('pk')
+        if not componente_id:
+            return Response({'error': 'Debe proporcionar el ID del componente'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            detalle_tecnologico = DetalleTecnologico.objects.get(det_tec_com_uso=componente_id)
+            detalle_tecnologico.det_tec_com_uso = None
+            detalle_tecnologico.save()
+            return Response({'Componente': 'Componente quitado'}, status=status.HTTP_200_OK)
+        except DetalleTecnologico.DoesNotExist:
+            return Response({'error': 'Componente no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    @action(detail=False, methods=['patch'])
+    def repotenciar(self, request):
+        componente = request.data.get('componente_id')
+        det_tec_id = request.data.get('det_tec_id')
+
+        if not componente or not det_tec_id:
+            return Response({'error': 'Debe proporcionar ambos valores'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            DetalleTecnologico.objects.filter(det_tec_com_uso_id=componente).update(det_tec_com_uso_id=None)
+            DetalleTecnologico.objects.filter(det_tec_id=det_tec_id).update(det_tec_com_uso_id=componente)
+            return Response({
+                'actualizad': 'actualizado',
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ComponenteDetalleView(APIView):
     def get(self, request):
